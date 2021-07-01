f77790eedc0a0d63a184cbd12fb8db1328b98c62
The Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-30 22:47:44 -0500

    initial commit

---
+++ src/components/PageEl.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.main`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* border: 2px solid dodgerblue; */
+`;
+
+const Title = styled.h1`
+  /* border: 1px dashed green; */
+`;
+
+export default function PageEl() {
+  return (
+    <Main>
+      <Title>Page component</Title>
+    </Main>
+  );
+}
